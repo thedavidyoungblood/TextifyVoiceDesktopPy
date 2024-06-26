7a83effdb80dc04c474f135863f5ac7f5a0ceaed
Felipe <lipefelipe403@hotmail.com>
2024-06-26 14:17:53 -0300

    Change how to load the Whisper model, now the model is in a local directory

--- main.py
+++ main.py
@@ -9,9 +9,17 @@ import webbrowser
 from plyer import notification
 import winsound
 import logging
+import json
 
 warnings.filterwarnings("ignore", category=FutureWarning, message="FP16 is not supported on CPU; using FP32 instead")
 
+CONFIG_FILE = "config.json"
+config = {}
+try:
+    with open(CONFIG_FILE, 'r') as f:
+        config = json.load(f)
+except FileNotFoundError:
+    pass
 
 def configurar_logger():
     if not os.path.exists("logs"):
@@ -23,44 +31,55 @@ configurar_logger()
 
 cancelar_desgravacao = False
 
-def extrair_e_transcrever(filepaths, text_var, btn_abrir, btn_select):
+def extrair_e_transcrever(filepaths, text_var, btn_abrir, btn_select, model_path):
     global cancelar_desgravacao
-    
-    model = whisper.load_model("large")
+
+    try:
+        model = whisper.load_model(model_path)
+        logging.info(f"Modelo transcrição:{model}")
+    except Exception as e:
+        logging.error(f"Erro ao carregar o modelo de transcrição: {e}")
+        text_var.set(f"Erro ao carregar o modelo. Verifique o caminho.")
+        return
 
     for filepath in filepaths:
+        logging.info(f"Analisando arquivo: {filepath}")
         text_var.set("Processando arquivos...")
 
         if cancelar_desgravacao:
             text_var.set("Desgravação cancelada. Selecione arquivos para começar.")
-            btn_select.config(text="Selecionar Arquivos e Local de Salvamento", command=lambda: iniciar_processo(btn_abrir, btn_select))
+            btn_select.config(text="Selecionar Arquivos e Local de Salvamento",
+                             command=lambda: iniciar_processo(btn_abrir, btn_select, btn_modelo))
+            btn_modelo.config(state=tk.NORMAL)  # Reativa o botão de modelo
             cancelar_desgravacao = False
             logging.info("Desgravação cancelada pelo usuário.")
             return
-        
+
         nome_arquivo = os.path.splitext(os.path.basename(filepath))[0]
         local_salvamento = os.path.join(os.path.dirname(filepath), nome_arquivo + "-transcrição.docx")
-        
+
         try:
             text_var.set(f"Desgravando: {nome_arquivo} ⏳ Por favor, aguarde.")
             logging.info(f"Iniciando transcrição do arquivo: {filepath}")
-            
+
             result = model.transcribe(filepath)
-            
+
             doc = Document()
-            
+
             for segment in result["segments"]:
                 text = segment["text"]
                 doc.add_paragraph(text)
-            
+
             doc.save(local_salvamento)
             logging.info(f"Transcrição concluída e salva em: {local_salvamento}")
-        
+
         except Exception as e:
             logging.error(f"Erro ao transcrever {nome_arquivo}. Motivo: {e}")
             cancelar_desgravacao = True
             text_var.set(f"Erro no desgravando {nome_arquivo}. Motivo: {e}")
-            btn_select.config(text="Selecionar Arquivos e Local de Salvamento", command=lambda: iniciar_processo(btn_abrir, btn_select))
+            btn_select.config(text="Selecionar Arquivos e Local de Salvamento",
+                             command=lambda: iniciar_processo(btn_abrir, btn_select, btn_modelo))
+            btn_modelo.config(state=tk.NORMAL)  # Reativa o botão de modelo
             notification.notify(
                 title="Erro na Transcrição",
                 message=f"Ocorreu um erro ao transcrever {nome_arquivo}.",
@@ -68,11 +87,13 @@ def extrair_e_transcrever(filepaths, text_var, btn_abrir, btn_select):
             )
             winsound.MessageBeep(winsound.MB_ICONHAND)
             return
-        
+
     text_var.set("Todas as transcrições foram concluídas.")
-    btn_select.config(text="Selecionar Arquivos e Local de Salvamento", command=lambda: iniciar_processo(btn_abrir, btn_select))
+    btn_select.config(text="Selecionar Arquivos e Local de Salvamento",
+                     command=lambda: iniciar_processo(btn_abrir, btn_select, btn_modelo))
+    btn_modelo.config(state=tk.NORMAL)  # Reativa o botão de modelo
     btn_abrir.config(state=tk.NORMAL, command=lambda: abrir_local_salvamento(filepaths))
-    
+
     logging.info("Todas as transcrições foram concluídas com sucesso.")
     notification.notify(
         title="Transcrição Concluída",
@@ -87,34 +108,47 @@ def abrir_local_salvamento(filepaths):
         webbrowser.open(diretorio)
         logging.info(f"Abrindo diretório de salvamento: {diretorio}")
 
-def iniciar_transcricao_thread(filepaths, text_var, btn_abrir, btn_select):
-    Thread(target=lambda: extrair_e_transcrever(filepaths, text_var, btn_abrir, btn_select)).start()
+def iniciar_transcricao_thread(filepaths, text_var, btn_abrir, btn_select, model_path):
+    Thread(target=lambda: extrair_e_transcrever(filepaths, text_var, btn_abrir, btn_select, model_path)).start()
 
-def selecionar_arquivo_e_salvar(text_var, btn_select, btn_abrir):
+def selecionar_arquivo_e_salvar(text_var, btn_select, btn_abrir, btn_modelo, model_path):
     global cancelar_desgravacao
-    filepaths = filedialog.askopenfilenames(title="Escolher os vídeos que serão transcritos para texto", filetypes=[("MP4 files", "*.mp4"), ("MP3 files", "*.mp3")])
+    filepaths = filedialog.askopenfilenames(title="Escolher os vídeos que serão transcritos para texto",
+                                            filetypes=[("MP4 files", "*.mp4"), ("MP3 files", "*.mp3")])
     if not filepaths:
         text_var.set("Seleção de arquivo cancelada. Operação interrompida.")
         logging.info("Seleção de arquivo cancelada pelo usuário.")
         return None
 
     text_var.set(f"{len(filepaths)} arquivo(s) selecionado(s) para transcrição.")
-    btn_select.config(text="Cancelar desgravação", command=lambda: cancelar_desgravacao_fn(btn_select))
+    btn_select.config(text="Cancelar desgravação", command=lambda: cancelar_desgravacao_fn(btn_select, btn_modelo))
+    btn_modelo.config(state=tk.DISABLED)  # Desativa o botão de modelo durante a transcrição
     btn_abrir.config(state=tk.DISABLED)
     logging.info(f"{len(filepaths)} arquivo(s) selecionado(s) para transcrição.")
     return filepaths
 
-def cancelar_desgravacao_fn(btn_select):
+def cancelar_desgravacao_fn(btn_select, btn_modelo):
     global cancelar_desgravacao
     cancelar_desgravacao = True
-    btn_select.config(text="Selecionar Arquivos e Local de Salvamento", command=lambda: iniciar_processo(btn_abrir, btn_select))
+    btn_select.config(text="Selecionar Arquivos e Local de Salvamento",command=lambda: iniciar_processo(btn_abrir, btn_select, btn_modelo))
+    btn_modelo.config(state=tk.NORMAL)  # Reativa o botão de modelo
     text_var.set("Cancelamento em processo...")
     logging.info("Processo de desgravação cancelado pelo usuário.")
+
+def selecionar_modelo():
+    global config
+    filepath = filedialog.askopenfilename(title="Selecionar Modelo Whisper",filetypes=[("Modelo Whisper", "*.pt;*.bin")])
+    if filepath:
+        config['model_path'] = filepath
+        with open(CONFIG_FILE, 'w') as f:
+            json.dump(config, f)
+        model_path_var.set(filepath)
+        logging.info(f"Caminho do modelo atualizado: {filepath}")
 
 root = tk.Tk()
 root.title("Desgravador [ Beta ]")
 
-root.geometry("650x450")
+root.geometry("650x500")
 
 cor_fundo = "#343a40"
 root.configure(bg=cor_fundo)
@@ -124,9 +158,12 @@ style.theme_use('clam')
 
 cor_frente = "#f8f9fa"
 cor_acento = "#007bff"
+cor_modelo = "#28a745"  # Cor verde para o botão de modelo
 
 style.configure("TFrame", background=cor_fundo)
 style.configure("TButton", background=cor_acento, foreground=cor_frente, font=("Arial", 10, "bold"), borderwidth=1)
+style.configure("Modelo.TButton", background=cor_modelo, foreground=cor_frente, font=("Arial", 10, "bold"),
+                borderwidth=1)  # Estilo para o botão de modelo
 style.configure("TLabel", background=cor_fundo, foreground=cor_frente, font=("Arial", 12))
 style.configure("Title.TLabel", background=cor_fundo, foreground=cor_frente, font=("Arial", 16, "bold"))
 
@@ -135,7 +172,7 @@ title_frame.pack(side=tk.TOP, fill=tk.X)
 title_frame.pack_propagate(False)
 
 titulo = ttk.Label(title_frame, text="Transcritor de Áudio", style="Title.TLabel", anchor="center")
-titulo.pack(side=tk.TOP, fill=tk.X,pady=20)
+titulo.pack(side=tk.TOP, fill=tk.X, pady=20)
 
 frame = ttk.Frame(root, style="TFrame")
 frame.pack(expand=True, fill=tk.BOTH, padx=20, pady=20)
@@ -145,16 +182,24 @@ text_var.set("Selecione os arquivos MP4 para transcrever.")
 text_label = ttk.Label(frame, textvariable=text_var, wraplength=550, style="TLabel")
 text_label.pack()
 
+# Carregar caminho do modelo das configurações ou definir um padrão
+model_path = config.get('model_path')
+model_path_var = tk.StringVar(value=model_path)
+
 btn_abrir = ttk.Button(frame, text="Abrir Pasta de Documentos Transcritos", state=tk.DISABLED, style="TButton")
 btn_select = ttk.Button(frame, text="Selecionar Arquivos e Local de Salvamento", style="TButton")
+btn_modelo = ttk.Button(frame, text="Selecionar qualidade", command=selecionar_modelo, style="Modelo.TButton") # Aplica o estilo ao botão
+model_path_label = ttk.Label(frame, textvariable=model_path_var, wraplength=550, style="TLabel")
 
-def iniciar_processo(btn_abrir, btn_select):
-    filepaths = selecionar_arquivo_e_salvar(text_var, btn_select, btn_abrir)
+def iniciar_processo(btn_abrir, btn_select, btn_modelo):
+    filepaths = selecionar_arquivo_e_salvar(text_var, btn_select, btn_abrir, btn_modelo, model_path_var.get())
     if filepaths:
-        iniciar_transcricao_thread(filepaths, text_var, btn_abrir, btn_select)
+        iniciar_transcricao_thread(filepaths, text_var, btn_abrir, btn_select, model_path_var.get())
 
-btn_select.config(command=lambda: iniciar_processo(btn_abrir, btn_select))
+btn_select.config(command=lambda: iniciar_processo(btn_abrir, btn_select, btn_modelo))
 btn_select.pack(pady=(10, 0))
 btn_abrir.pack(pady=(10, 20))
+btn_modelo.pack(pady=(10, 0))
+model_path_label.pack()
 
 root.mainloop()
